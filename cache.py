15542d188800ee0f1071ac2d67a1bb40d82c0b7b
Xiyuan Liu <mail@mona.moe>
2022-04-05 22:26:14 -0400

    build up skeleton

---
+++ cache.py
@@ -0,0 +1,38 @@
+import json
+
+CACHE_FILENAME = "cache.json"
+
+def open_cache():
+    ''' opens the cache file if it exists and loads the JSON into
+    a dictionary, which it then returns.
+    if the cache file doesn't exist, creates a new cache dictionary
+    Parameters
+    ----------
+    None
+    Returns
+    -------
+    The opened cache
+    '''
+    try:
+        cache_file = open(CACHE_FILENAME, 'r')
+        cache_contents = cache_file.read()
+        cache_dict = json.loads(cache_contents)
+        cache_file.close()
+    except:
+        cache_dict = {}
+    return cache_dict
+
+def save_cache(cache_dict):
+    ''' saves the current state of the cache to disk
+    Parameters
+    ----------
+    cache_dict: dict
+        The dictionary to save
+    Returns
+    -------
+    None
+    '''
+    dumped_json_cache = json.dumps(cache_dict)
+    fw = open(CACHE_FILENAME,"w")
+    fw.write(dumped_json_cache)
+    fw.close()
---
+++ main.py
@@ -0,0 +1,13 @@
+import cache
+
+def request_nrel_api(term):
+    pass
+
+def request_googlemap_api(term):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
