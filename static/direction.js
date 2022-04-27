4c4f3c1764098e812cc3838e5af94d9cfee4bda0
Xiyuan Liu <mail@mona.moe>
2022-04-27 19:09:40 -0400

    finished direction

--- static/direction.js
+++ static/direction.js
@@ -2,29 +2,20 @@ function initMap() {
     const directionsService = new google.maps.DirectionsService();
     const directionsRenderer = new google.maps.DirectionsRenderer();
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 7,
-      center: { lat: 41.85, lng: -87.65 },
+      zoom : 3,
+      center: orig,
     });
   
     directionsRenderer.setMap(map);
   
-    const onChangeHandler = function () {
-      calculateAndDisplayRoute(directionsService, directionsRenderer);
-    };
-  
-    document.getElementById("start").addEventListener("change", onChangeHandler);
-    document.getElementById("end").addEventListener("change", onChangeHandler);
+    calculateAndDisplayRoute(directionsService, directionsRenderer);
   }
   
   function calculateAndDisplayRoute(directionsService, directionsRenderer) {
     directionsService
       .route({
-        origin: {
-          query: document.getElementById("start").value,
-        },
-        destination: {
-          query: document.getElementById("end").value,
-        },
+        origin: orig,
+        destination: dest,
         travelMode: google.maps.TravelMode.DRIVING,
       })
       .then((response) => {
